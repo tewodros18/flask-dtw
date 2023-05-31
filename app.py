0908fb467e61738639e5c7b62a8dfa695a2ffec1
Tewodros M <58773098+tewodros18@users.noreply.github.com>
2023-05-31 14:24:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import numpy as np
+import pandas as pd
+from fastdtw import fastdtw
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/dtw')
+def dtw():
+    x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
+    y = np.array([[2,2], [3,3], [4,4]])
+    distance, path = fastdtw(x, y)
+    return distance
