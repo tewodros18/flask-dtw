d841c78fa3eb41b2839d81a9e4aabd082a03a28b
Tewodros M <58773098+tewodros18@users.noreply.github.com>
2023-05-31 14:59:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/dtw')
+@app.route('/dtw', methods=["POST"])
 def dtw():
-    x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
-    y = np.array([[2,2], [3,3], [4,4]])
+    #x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
+    x = request.json["list1"]
+    #y = np.array([[2,2], [3,3], [4,4]])
+    y = request.json["list2"]
     distance, path = fastdtw(x, y)
     return str(distance)
