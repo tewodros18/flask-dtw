12a74623049ad32b821eb5269fef7a6c147cd1f1
Tewodros M <58773098+tewodros18@users.noreply.github.com>
2023-06-03 09:46:20 +0300

    Update sign_recorder.py

--- sign_recorder.py
+++ sign_recorder.py
@@ -55,7 +55,7 @@ class SignRecorder(object):
         self.recorded_results = []
 
 
-    def _get_sign_predicted(self, batch_size=5, threshold=0.6):
+    def _get_sign_predicted(self, batch_size=5, threshold=0.5):
         """
         Method that outputs the sign that appears the most in the list of closest
         reference signs, only if its proportion within the batch is greater than the threshold
